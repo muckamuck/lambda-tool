65038217ed7eff0a233f90c7eaa825a0c9e3db7c
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-11-08 17:29:00 -0600

    Steps toward including the template in the wheel

--- setup.py
+++ setup.py
@@ -1,16 +1,36 @@
 from setuptools import setup
+import os
+
+
+def find_data(starting_dir, the_dir):
+    original_cwd = os.getcwd()
+    tree = []
+    try:
+        os.chdir(starting_dir)
+        for folder, subs, files in os.walk(the_dir):
+            for file in files:
+                tree.append('{}/{}'.format(folder, file))
+    except Exception:
+        pass
+
+    os.chdir(original_cwd)
+    return tree
+
+
 setup(
-    name="LambdaTool",
-    version="0.0.0",
+    name='LambdaTool',
+    version='0.0.0',
     packages=['lambdatool'],
     description='Python Lambda utility',
     author='Chuck Muckamuck',
     author_email='Chuck.Muckamuck@gmail.com',
+    include_package_data=True,
+    package_data={'lambdatool': find_data('lambdatool', 'template')},
     install_requires=[
-        "boto3>=1.4.3",
-        "Click>=6.7",
-        "PyYAML>=3.12",
-        "pymongo>=3.4.0"
+        'boto3>=1.4.3',
+        'Click>=6.7',
+        'PyYAML>=3.12',
+        'pymongo>=3.4.0'
     ],
     entry_points="""
         [console_scripts]
