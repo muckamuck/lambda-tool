2ca12cde5378167cfa70d6794a2cd6a3d3cfa037
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-11-13 23:13:18 +0000

    first

---
+++ lambdatool/utility.py
@@ -0,0 +1,19 @@
+import boto3
+import traceback
+import sys
+import logging
+
+
+def get_api_client(profile_name, region_name, aws_service):
+    try:
+        if profile_name:
+            api_session = boto3.Session(profile_name=profile_name, region_name=region_name)
+        else:
+            api_session = boto3.Session(region_name=region_name)
+
+        api_client = api_session.client(aws_service)
+        return api_client
+    except Exception as x:
+        logging.error('Exception caught in get_api_client(): {}'.format(x))
+        traceback.print_exc(file=sys.stdout)
+        return None
