199a6b82c98f5e1b4b30100dccc5dde0763c0027
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-11-13 20:51:01 +0000

    Walk the directory to add everything

--- lambdatool/template/util/zipper.py
+++ lambdatool/template/util/zipper.py
@@ -1,3 +1,4 @@
+import os
 import zipfile
 try:
     import zlib #noqa
@@ -10,12 +11,30 @@ modes = {
     zipfile.ZIP_STORED:   'stored'
 }
 
-print('creating archive')
-zf = zipfile.ZipFile('zipfile_write_compression.zip', mode='w')
-try:
-    print('adding files with compression mode={}'.format(modes[compression]))
-    zf.write('x', compress_type=compression)
-    zf.write('a/x', compress_type=compression)
-finally:
-    print('closing')
-    zf.close()
+
+def create_zipfile():
+    print('creating archive')
+    zf = zipfile.ZipFile('/tmp/zipfile_write_compression.zip', mode='w')
+    try:
+        print('adding files with compression mode={}'.format(modes[compression]))
+        zf.write('.', compress_type=compression)
+        for f in find_data('.'):
+            zf.write(f, compress_type=compression)
+    finally:
+        print('closing')
+        zf.close()
+
+
+def find_data(the_dir):
+    tree = []
+    try:
+        for folder, subs, files in os.walk(the_dir):
+            for file in files:
+                tree.append('{}/{}'.format(folder, file))
+    except Exception:
+        pass
+
+    return tree
+
+if __name__ == '__main__':
+    create_zipfile()
