35c3861e548b5f29b50ca01a517a8e3e2eea0b7a
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-12-02 08:59:05 -0600

    new main file for the flask service

--- lambdatool/template/main.py
+++ lambdatool/template/main.py
@@ -1,12 +1,22 @@
-from utility import add_cors
+from utility import FlaskLambda
+
+app = FlaskLambda(__name__)
 
 
-@add_cors
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': '42',
-        'headers': {
-            'Content-Type': 'text/plain'
-        }
+@app.route('/foo', methods=['GET', 'POST'])
+def foo():
+    data = {
+        'form': request.form.copy(),
+        'args': request.args.copy(),
+        'json': request.json
     }
+    return (
+        json.dumps(data, indent=4, sort_keys=True),
+        200,
+        {'Content-Type': 'application/json'}
+    )
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
