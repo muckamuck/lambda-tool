2be95846ac0273ba0e1bb89495c739142f2d6c29
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-12-09 15:38:24 -0600

    Bump version: 0.1.4 → 0.1.5

--- lambdatool/command.py
+++ lambdatool/command.py
@@ -24,7 +24,7 @@ lambdatool deploy. The lambda has been started in main.py.
 
 
 @click.group()
-@click.version_option(version='0.1.4')
+@click.version_option(version='0.1.5')
 def cli():
     pass
 
