4d4fcac69fbec0b99b6e6f4620feb95924b0c25c
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2017-12-09 15:09:02 -0600

    Bump version: 0.1.3 → 0.1.4

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def find_data(starting_dir, the_dir):
 
 setup(
     name='LambdaTool',
-    version='0.1.3',
+    version='0.1.4',
     packages=['lambdatool'],
     description='Python Lambda utility',
     author='Chuck Muckamuck',
