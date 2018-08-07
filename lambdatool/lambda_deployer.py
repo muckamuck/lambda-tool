b70de6691d756ef15ea1bd584c7a969a818469b6
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2018-08-07 08:15:55 -0500

    Bump version: 0.5.1 → 0.5.2

--- lambdatool/lambda_deployer.py
+++ lambdatool/lambda_deployer.py
@@ -33,7 +33,7 @@ logging.basicConfig(level=logging.INFO,
 logging.getLogger().setLevel(logging.INFO)
 
 
-LAMBDATOOL_VERSION = '0.5.1'
+LAMBDATOOL_VERSION = '0.5.2'
 LAMBDATOOL_DESCRIPTOR = '.lambdatool'
 DEFAULT_DESCRIPTION = 'Fantastic Lambda Function'
 DEFAULT_MODULE_FILE = 'main.py'
