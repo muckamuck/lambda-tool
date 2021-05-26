3b9cb51aa8b4c8a24ec5665415d3daca518e10da
Chuck Muckamuck (pluto) <chuck.muckamuck@gmail.com>
2021-05-26 10:25:48 -0500

    Bump version: 0.9.0 → 0.9.1

--- lambdatool/lambda_deployer.py
+++ lambdatool/lambda_deployer.py
@@ -37,7 +37,7 @@ logging.basicConfig(level=logging.INFO,
 logging.getLogger().setLevel(logging.INFO)
 
 
-LAMBDATOOL_VERSION = '0.9.0'
+LAMBDATOOL_VERSION = '0.9.1'
 LAMBDATOOL_DESCRIPTOR = '.lambdatool'
 DEFAULT_DESCRIPTION = 'Fantastic Lambda Function'
 DEFAULT_MODULE_FILE = 'main.py'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def find_data(starting_dir, the_dir):
 
 setup(
     name='LambdaTool',
-    version='0.9.0',
+    version='0.9.1',
     packages=['lambdatool'],
     description='Python Lambda utility',
     author='Chuck Muckamuck',
