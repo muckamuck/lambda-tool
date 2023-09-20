0e4a022064a3c2420365aad170ebb861edaf3a8b
Chuck Muckamuck <chuck.muckamuck@gmail.com>
2023-09-20 14:00:49 -0500

    version stub work

--- lambdatool/lambda_deployer.py
+++ lambdatool/lambda_deployer.py
@@ -38,7 +38,7 @@ logging.getLogger().setLevel(logging.INFO)
 
 
 MINOR_VERSIONS = [6, 7, 8, 9, 10, 11]
-LAMBDATOOL_VERSION = '0.9.9'
+LAMBDATOOL_VERSION = '0.9.9-pip-extras'
 LAMBDATOOL_DESCRIPTOR = '.lambdatool'
 DEFAULT_DESCRIPTION = 'Fantastic Lambda Function'
 DEFAULT_MODULE_FILE = 'main.py'
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def find_data(starting_dir, the_dir):
 
 setup(
     name='LambdaTool',
-    version='0.9.9',
+    version='0.9.9-pip-extras',
     packages=['lambdatool'],
     description='Python Lambda utility',
     author='Chuck Muckamuck',
